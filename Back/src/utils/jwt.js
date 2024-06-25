0eb31712fbb487f77b97955eec74887d386fad9f
IHCantabraba <inigocanta@gmail.com>
2024-06-25 22:02:36 +0200

    tokenExpiredIn

--- Back/src/utils/jwt.js
+++ Back/src/utils/jwt.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken')
 
 const generateKey = (_id) => {
-  return jwt.sign({ _id }, process.env.SECRET_KEY, { expiresIn: 5 * 60 })
+  return jwt.sign({ _id }, process.env.SECRET_KEY, { expiresIn: 60 * 60 })
 }
 const verifyToken = (token) => {
   return jwt.verify(token, process.env.SECRET_KEY)
